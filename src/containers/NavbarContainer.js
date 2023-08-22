f698012aa83d481c3445a768e99c3f450b698e79
AlexandraOlariu11 <elenaalexandraolariu@gmail.com>
2023-08-22 15:14:25 +0300

    add navbar

--- src/containers/NavbarContainer.js
+++ src/containers/NavbarContainer.js
@@ -1,5 +1,26 @@
-import React from "react";
+//import React from "react";
 //import logo from 
-import { Link, useLocation } from "react-router-dom";
+//import { Link, useLocation } from "react-router-dom";
 
 //const NavbarContainer = ({  }) => { }
+
+export default function NavbarContainer () {
+    return (
+    <nav className="nav">
+        <a href="/" className="title">
+            PetVoyage
+        </a>
+        <ul>
+            <li className="active">
+                <a href="/home">Home</a>
+            </li>
+            <li>
+                <a href="/home">About</a>
+            </li>
+            <li>
+                <a href="/home">Contact</a>
+            </li>
+        </ul>
+    </nav>
+    )
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from "react";
-import mockData from "../assets/mock";
+//import React, { useEffect, useState } from "react";
+//import mockData from "../assets/mock";
 import NavbarContainer from  "../containers/NavbarContainer";
-import PopularDestinationsHP from "../containers/PopularDestinationsHP";
-import HowItWorksContainer from "../containers/HowItWorksContainer";
-import ContentHomepageContainer from "../containers/ContentHomepageContainer";
-import ImagesContainerHP from "../containers/ImagesContainerHP";
-import FooterContainer from "../containers/FooterContainer";
-import TitleComponent from "../components/TitleComponent";
+import "./NavStyles.css";
+
+//import PopularDestinationsHP from "../containers/PopularDestinationsHP";
+//import HowItWorksContainer from "../containers/HowItWorksContainer";
+//import ContentHomepageContainer from "../containers/ContentHomepageContainer";
+//import ImagesContainerHP from "../containers/ImagesContainerHP";
+//import FooterContainer from "../containers/FooterContainer";
+//import TitleComponent from "../components/TitleComponent";
 
 function HomePage () {
-    return (
-      <div className="container"> 
-      <h1>PetVoyage - Descoperă localuri pet-friendly în călătoriile tale!</h1>;
-       <TitleComponent/>
-    </div>
-     );
+    return <NavbarContainer/>
+    //<h1>PetVoyage - Descoperă localuri pet-friendly în călătoriile tale!</h1>;
+
 };
 
 
